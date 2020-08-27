7a65f1be6719f1e93bd7091e07e407f2712ac1af
Royette Miranda <Royettemiranda@YAHOO.COM>
2020-08-28 02:10:05 +0800

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -1,52 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-	int num;
-	struct node* next;
-}node;
-
-void print_and_free_list(node* head);
-
-int main(void)
+// Defines what a node is
+typedef struct node
 {
-    int nums;
-	node* temp = NULL;
-	node* head = NULL;
-	node* c = NULL;
+    int value;
+    struct node *next;
+} node;
 
-    printf("How many Numbers?: ");
-    scanf("%i", &nums);
-	for (int i = 0; i < nums; i++)
-	{
-		temp = malloc(sizeof(node));
-		if (temp == NULL){
-		    printf("Unable to Allocate Memory");
-		    return 1;
-		}
-		temp->next = NULL;
-		scanf("%i", &temp->num);
+// Prototypes
+node *createlist(int size);
+void printlist(node *head);
+void freelist(node *head);
 
-		if (head == NULL)
-		{
-			head = temp;
-			c = head;
-		}
-		else {
-			c->next = temp;
-			c = c->next;
-		}
-	}
-    print_and_free_list(head);
+// I just realized I made it unnecessarily complicated
+int main(int argc, char *argv[])
+{
+    int num = strtol(argv[1], NULL, 10);
+    char *var = getenv("NAME");
+
+    // Checks if the environment variable exists first
+    if (var){
+        printf_s("Hello, %s, for some reason you wanted to enter %s numbers\n",  var, argv[1]);
+    }
+
+    node *list1 = createlist(num);
+    if (!list1){
+        return 1;
+    }
+
+    printf_s("YOU ENTERED THESE NUMBERS: ");
+    printlist(list1);
+    freelist(list1);
+    return 0;
 }
 
-void print_and_free_list(node* head)
-{
-    if (head == NULL) {
-	return;
-	}
 
-	printf("%i \n", head->num);
-	free(head);
-	print_and_free_list(head->next);
+// Returns a list of size n
+node *createlist(int n)
+{
+    // Initializes the pointers (points them to null)
+    node *head = NULL,
+         *c = NULL;
+
+    for (int i = 0; i < n; i++)
+    {
+        node *temp = malloc(sizeof(node));
+        if (temp == NULL)
+        {
+            return NULL;
+        }
+        temp->next = NULL;
+
+        printf_s("ENTER THE DESIRED AMOUNT for number %i: ", i + 1);
+        scanf("%i", &temp->value);
+
+        if (head == NULL)
+        {
+            head = temp;
+            c = head;
+        }
+        else
+        {
+            c->next = temp;
+            c = c->next;
+        }
+    }
+    return head;
+}
+
+
+// Prints the list
+void printlist(node *head)
+{
+    if (head == NULL){
+        return;
+    }
+    printf_s("%i ", head->value);
+    printlist(head->next);
+}
+
+
+// Frees em
+void freelist(node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    freelist(head->next);
+    free(head);
 }
